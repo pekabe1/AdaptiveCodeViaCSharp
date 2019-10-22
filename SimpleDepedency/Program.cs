04b93b4e8e269dd66a22ead148ee90cbbc864321
Kacper <Kacper@DESKTOP-CDNO7J9>
2019-10-22 13:13:29 +0200

    Add project files.

---
+++ SimpleDepedency/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MessagePrinter;
+
+namespace SimpleDepedency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new PrintService();
+            service.PrintMessage();
+            Console.Read();
+        }
+    }
+}
+// REMEMBER THAT POORLY MANAGED DEPENDECNIES QUCIK ESCALATE TO BIG ARCHITECTURAL PROBLEMS
